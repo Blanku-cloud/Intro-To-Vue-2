4fe815c07a2300a21b98620c4fad934cd3d19b74
Blanku-cloud <pokemondinner321@gmail.com>
2022-01-24 08:02:07 -0500

    test

---
+++ vue-basics/src/components/Button1.vue
@@ -0,0 +1,38 @@
+<template>
+  <button><slot></slot></button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+};
+</script>
+
+<style scoped>
+button {
+  background: blue;
+}
+/* button {
+  border: none;
+  background-color: transparent;
+  font-family: inherit;
+  padding: 0;
+  cursor: pointer;
+}
+
+a,
+button {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #1572a1;
+  color: #fff;
+  border-radius: 8px;
+  box-shadow: 0 3px 5px rgba(0, 0, 0, 0.18);
+  padding: 0.25em 0.75em;
+  min-width: 10ch;
+  min-height: 44px;
+  text-align: center;
+  line-height: 1.1;
+} */
+</style>
--- vue-basics/src/views/Home.vue
+++ vue-basics/src/views/Home.vue
@@ -1,16 +1,32 @@
 <template>
   <div class="home">
-    <h1>Hello</h1>
+    <h1>{{ now }}</h1>
+    <Button v-for="pokemonlol in pokemons" :key="pokemonlol">{{
+      pokemonlol
+    }}</Button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import Button from "../components/Button1.vue";
 export default {
   name: "Home",
-  components: {},
+  components: {
+    Button,
+  },
+  data() {
+    return {
+      pokemons: ["Pikachu", "Eevee", "Garchomp", "Gardevoir"],
+    };
+  },
+  computed: {
+    now: function () {
+      const time = new Date();
+      return `${time.getHours()}:${time.getMinutes()}`;
+    },
+  },
+  methods: {},
 };
 </script>
-<style>
-</style>
+<style></style>
